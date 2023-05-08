7e7c99bfe22a9b64a8490731d338bcccf5556273
Emerzon Javier Kolki Martinez <emerzonkolki@gmail.com>
2023-05-08 18:59:32 -0500

    feat: query client provider and devtools

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,17 @@ import { RouterProvider } from 'react-router-dom'
 
 import router from './router'
 
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const client = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={client}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>
 )
