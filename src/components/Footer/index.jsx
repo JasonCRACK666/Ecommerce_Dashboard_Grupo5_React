f2189c51a6703ac828079ec10682e6eb3318b978
Yhonel Espinola <osmeryhonel890@gmail.com>
2023-06-06 04:02:20 -0500

    feat: Add componet Footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,92 @@
+function Footer() {
+  return (
+    <div>
+      <footer>
+        <div className='row p-5 bg-dark text-white'>
+          <div className='col-xs-12 col-xs-6 col-lg-4'>
+            <p className='h2 text-center'>TECH HOUSE</p>
+            <p className='h7 font-monospace text-start text-secondary'>
+              Somos una empresa enfocada en la venta de productos de tecnología
+              como lo son los celulares, las laptops, las tables y asi mucho
+              más, nosotros lo que buscamos con nuestra empresa es que las
+              personas estén más cerca de adquirir algo nuevo para ellos con
+              solo darle click a un botón. Por otro lado, Buscamos mejorar la
+              compra de productos en línea, garantizando una buena experiencia a
+              nuestros usuarios.
+            </p>
+          </div>
+          <div className='col-xs-10 col-xs-5 col-lg-2'>
+            <p className='h5 mb-3'>Marcas Populares</p>
+            <div className='mb-2'>
+              <a className='text-secondary text-decoration-none' href='#'>
+                SAMSUNG
+              </a>
+            </div>
+            <div className='mb-2'>
+              <a className='text-secondary text-decoration-none' href='#'>
+                HP
+              </a>
+            </div>
+            <div className='mb-2'>
+              <a className='text-secondary text-decoration-none' href='#'>
+                LENOVO
+              </a>
+            </div>
+            <div className='mb-2'>
+              <a className='text-secondary text-decoration-none' href='#'>
+                DELL
+              </a>
+            </div>
+            <div className='mb-2'>
+              <a className='text-secondary text-decoration-none' href='#'>
+                ASUS
+              </a>
+            </div>
+          </div>
+          <div className='col-xs-10 col-xs-5 col-lg-2'>
+            <p className='h5 mb-3'>Visita</p>
+            <div className='mb-2'>
+              <p className='text-secondary text-lg-start'>
+                AV. España 2739, Trujillo
+              </p>
+              <p className='text-secondary text-lg-start'>
+                AV. Borgoño 361, Trujillo
+              </p>
+            </div>
+          </div>
+          <div className='col-xs-10 col-xs-5 col-lg-2'>
+            <p className='h5 mb-3'>Redes sociales</p>
+            <div className='mb-2'>
+              <a className='text-secondary text-decoration-none' href='#'>
+                Facebook
+              </a>
+            </div>
+            <div className='mb-2'>
+              <a className='text-secondary text-decoration-none' href='#'>
+                Instagram
+              </a>
+            </div>
+            <div className='mb-2'>
+              <a className='text-secondary text-decoration-none' href='#'>
+                Twitter
+              </a>
+            </div>
+          </div>
+          <div className='col-xs-10 col-xs-5 col-lg-2'>
+            <p className='h5 mb-3'>Legal</p>
+            <div className='mb-2'>
+              <a className='text-secondary text-decoration-none' href='#'>
+                Terminos de Privacidad
+              </a>
+            </div>
+          </div>
+          <div className='col-xs-12'>
+            <p className='text-white text-center'>Tech House - 2023</p>
+          </div>
+        </div>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
