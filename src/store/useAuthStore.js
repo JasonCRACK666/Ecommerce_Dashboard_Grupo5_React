0ac3db5bde7d4fdadb22fa0c3099d53500c0a508
JasonCrk <emerzonkolki@gmail.com>
2023-06-10 09:36:39 -0500

    fix: default token and add setIsAuth

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -4,10 +4,11 @@ import { devtools } from 'zustand/middleware'
 
 export const useAuthStore = create()(
   devtools(set => ({
-    token: null,
+    token: localStorage.getItem('token'),
     user: null,
     isAuth: false,
     setUser: user => set(state => ({ ...state, user, isAuth: true })),
+    setIsAuth: isAuth => set(state => ({ ...state, isAuth })),
     setToken: token => set(state => ({ ...state, token })),
     logOut: () => set(() => ({ token: null, user: null, isAuth: false }))
   }))
