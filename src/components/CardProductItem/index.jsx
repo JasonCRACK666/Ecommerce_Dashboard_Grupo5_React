1f9dfd0e7ec06ccd8355c913476d0966c341d61c
EPPR27 <113651234+EPPR27@users.noreply.github.com>
2023-06-09 19:26:25 -0500

    delete implementation and complete cardProductItem

--- src/components/CardProductItem/index.jsx
+++ src/components/CardProductItem/index.jsx
@@ -1,24 +1,29 @@
-import '../CardProductItem/CardProductItem.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import { FaStar } from "react-icons/fa";
 
-const CardProductItem = ({srcImg, productName,originalPrice, discountRate, finalPrice, value}) => {
+const CardProductItem = ({images, title,originalPrice, discountRate, finalPrice, averageScore ,countReviews}) => {
+  const residuaryStar= 5 - averageScore
+  const yellow = 'text-warning'
+  const white = 'text-light'
   return(
-    <div className="card m-5">
-      <img src={srcImg} className="card-img-top" alt="productItem" />
+    <div className="card m-5" style={{width: '18rem'}}>
+      <img src={images} className="card-img-top" alt="productItem" />
       <div className="card-body bg-dark">
-        <h4 className="card-title text-light">{productName}</h4>
+        <h4 className="card-title text-light">{title}</h4>
         <div className='row'>
         <p className='col-3 justify-content-center align-items-center text-light'>S/{finalPrice}</p>
         {!discountRate && <p className='col-3 text-decoration-line-through text-secondary'>S/{originalPrice}</p>}
         </div>
+        <div>
+
+    </div>
         <div className='row'>
-        <FaStar className='col-2 text-warning' />
-        <FaStar className='col-2  text-warning' />
-        <FaStar className='col-2  text-warning' />
-        <FaStar className='col-2  text-warning' />
-        <FaStar className='col-2 text-secondary' />
-        <p className='col-2 text-light'>({value})</p>
+        {[...Array(averageScore)].map((_, index) => (
+        <FaStar key={index} className={`col-2 ${yellow}`}/>
+      ))}
+      {[...Array(residuaryStar)].map((_, index) => (
+        <FaStar key={index} className={`col-2 ${white}`}/>
+      ))}
+        <p className='col-2 text-light'>({countReviews})</p>
         </div>
       </div>
       
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,6 @@
-import CardProductItem from "../components/CardProductItem"
 const HomePage = () => {
   return (
     <div>
-      <CardProductItem
-      srcImg={"https://compuusa.com.pe/8883-large_default/motherboard-gigabyte-b560m-ds3h-svl-ddr4.jpg"}
-      productName='MotherBoard'
-      finalPrice='90'
-      originalPrice='100'
-      discountRate={false}
-      value={4}
-      />
     </div>
   )
 }
