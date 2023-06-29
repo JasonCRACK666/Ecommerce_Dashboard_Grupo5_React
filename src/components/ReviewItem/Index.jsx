34df8031d63f59d82e940bda738af6ce81fea1a4
JasonCrk <emerzonkolki@gmail.com>
2023-06-29 17:14:10 -0500

    refactor: rename ProductDetailsReseña to ReviewItem

--- src/components/ReviewItem/Index.jsx
+++ src/components/ReviewItem/Index.jsx
@@ -1,35 +1,27 @@
-import { Link } from 'react-router-dom'
 import { FaStar } from 'react-icons/fa'
 
-const ProductDetailsReseña = ({
-  id,
-  username,
-  avatar,
-  score,
-  comment,
-  createAt
-}) => {
+import imgAvatarDefault from '../../assets/default-avatar.jpg'
+
+const ReviewItem = ({ username, avatar, score, comment, createdAt }) => {
   const scoreStar = 5 - score
 
   return (
     <div className='card p-2'>
       <div className='row card-body'>
         <div className='col-md-1 pl-0'>
-          <Link to={`/product/${id}`}>
-            <img
-              src={avatar}
-              alt={username}
-              className='rounded-circle w-100'
-              style={{
-                aspectRatio: 1 / 1
-              }}
-            />
-          </Link>
+          <img
+            src={avatar ?? imgAvatarDefault}
+            alt={username}
+            className='rounded-circle w-100'
+            style={{
+              aspectRatio: 1 / 1
+            }}
+          />
         </div>
         <div className='col-md-11 p-0'>
           <div className=''>
             <h6 className='card-title fw-bold mb-0'>{username}</h6>
-            <p className='mb-0'>{createAt.toLocaleDateString()}</p>
+            <p className='mb-0'>{new Date(createdAt).toLocaleDateString()}</p>
             <div className='mb-2'>
               {[...Array(score)].map((_, index) => (
                 <FaStar key={index} className='text-warning fs-5' />
@@ -46,4 +38,4 @@ const ProductDetailsReseña = ({
   )
 }
 
-export default ProductDetailsReseña
+export default ReviewItem
